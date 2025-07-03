99c99b383e456941df0bcf6039bac7f78bbd30b6
jeevant010 <jeevantmudgil10@gmail.com>
2025-07-03 08:56:43 +0530

    media query also done

--- Frontend/src/AppMain.jsx
+++ Frontend/src/AppMain.jsx
@@ -3,13 +3,13 @@ import Navbar from './components/Navbar';
 
 export default function AppLayout({ children, currentPath }) {
   return (
-    <div className="flex w-full min-h-screen bg-black">
+    <div className="flex w-full min-h-screen">
       {/* Permanent Side Navigation */}
       <Navbar currentPath={currentPath} />
 
       {/* Main Content Area */}
-      <div className="flex-1 bg-white min-h-screen overflow-auto ml-80">
-        <main className="px-10 py-8">
+      <div className="flex-1 bg-white min-h-screen overflow-auto ml-1 mr-1 lg:ml-28">
+        <main className="px-1 py-8">
           {children}
         </main>
       </div>
